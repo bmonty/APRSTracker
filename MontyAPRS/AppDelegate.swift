c3cbef8565805f323444adb973b935cca40d3919
Benjamin Montgomery <bmontgom@montynet.org>
2018-09-20 17:20:48 -0500

    Initial Commit

---
+++ MontyAPRS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MontyAPRS
+//
+//  Created by Benjamin Montgomery on 9/20/18.
+//  Copyright © 2018 Benjamin Montgomery. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MontyAPRS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MontyAPRS
+//
+//  Created by Benjamin Montgomery on 9/20/18.
+//  Copyright © 2018 Benjamin Montgomery. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
